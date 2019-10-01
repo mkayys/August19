12dd6cb87d2d37f68fdd65d35c93fa61ab817746
Michelle Kim <x1michelleskim@gmail.com>
2019-10-01 10:30:18 -0700

    Refactor create action in users controller

--- 99Cats2/app/controllers/users_controller.rb
+++ 99Cats2/app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            
+            login(@user)
+            redirect_to cats_url
         else
             render json: @user.errors.full_messages, status: 418
         end
