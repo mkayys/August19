7597bc549550e23cf8a41c05257bd31473e8e648
Michelle Kim <x1michelleskim@gmail.com>
2019-10-01 10:59:44 -0700

    Finish user model with validations

--- RedemptionTunes/app/models/user.rb
+++ RedemptionTunes/app/models/user.rb
@@ -18,7 +18,28 @@ class User < ApplicationRecord
 
     attr_reader :password
 
-    def self.find_by_credentials(email, pw)
-        
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: SecureRandom.urlsafe_base64(16))
+        self.session_token
     end
 end
