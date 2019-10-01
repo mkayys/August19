e2e5fbd9be4e6dbb1b9da1882bbddf4c6bf5f522
Michelle Kim <x1michelleskim@gmail.com>
2019-10-01 11:05:07 -0700

    Write out skeleton for user and session controller

---
+++ RedemptionTunes/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user
+        else
+            render json: ['yo, wrong info'], status: 401
+        end
+    end
+
+    def destroy
+
+    end
+end
