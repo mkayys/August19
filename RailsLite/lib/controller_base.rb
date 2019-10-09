4a0fad04ef278eda2aba85cc125c4306f620fc4c
Michelle Kim <x1michelleskim@gmail.com>
2019-10-08 19:34:16 -0700

    Write in comments

--- RailsLite/lib/controller_base.rb
+++ RailsLite/lib/controller_base.rb
@@ -10,23 +10,33 @@ class ControllerBase
   def initialize(req, res)
     @req = req
     @res = res
-
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'no doubles' if already_built_response?
+    @already_built_response = true
+    # @res['Location'] = url
+    @res.set_header('Location', url)
+    @res.status = 302
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'you are wrong' if already_built_response?
+    @already_built_response = true
     @res.write(content)
     @res['Content-Type'] = content_type
+    @res.status = 200 #default: 200 so we don't have to write this
+    
   end
 
   # use ERB and binding to evaluate templates
