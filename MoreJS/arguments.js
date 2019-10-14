b25f41c881082ae58d66b0bc6a3f077f8df42eb8
Michelle Kim <x1michelleskim@gmail.com>
2019-10-14 10:31:41 -0700

    Finish bind with arguments

--- MoreJS/arguments.js
+++ MoreJS/arguments.js
@@ -10,9 +10,78 @@ function sum() {
 }
 
 // with rest operator
-// function sum() {
-
-// }
+function sum2(...args) {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
 
 console.log(sum(1, 2, 3, 4)) // 10
-console.log(sum(1, 2, 3, 4, 5)) // 15
+console.log(sum2(1, 2, 3, 4, 5)) // 15
+
+
+
+// bind with args
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    says(sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind2(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
+
+Function.prototype.myBind = function(ctx) {
+    let bindArgs = Array.from(arguments);
+    const that = this;
+    return function _myBind() {
+        let callArgs = Array.from(arguments);
+        return that.apply(ctx, bindArgs.concat(callArgs));
+    }
+
+}
+
+Function.prototype.myBind2 = function(ctx, ...bindArgs) {
+    const that = this;
+    return function _myBind2(...callArgs) {
+        return that.apply(ctx, bindArgs.concat(callArgs));
+    }
+}
