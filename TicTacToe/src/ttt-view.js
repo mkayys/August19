da2ffa912c99050717a4da5e1dd6bb6489560c3e
Michelle Kim <x1michelleskim@gmail.com>
2019-10-16 11:24:44 -0700

    Resize board and add data attribute to list items appended to ul

--- TicTacToe/src/ttt-view.js
+++ TicTacToe/src/ttt-view.js
@@ -7,7 +7,7 @@ class View {
   }
 
   bindEvents() {
-
+    
   }
 
   makeMove($square) {}
@@ -19,6 +19,18 @@ class View {
 
     for (let i = 0; i < 9; i++) {
       let $li = $('<li>');
+
+      let xPos;
+      if (i < 3) {
+        xPos = 0;
+      } else if (i >= 3 && i < 6) {
+        xPos = 1;
+      } else {
+        xPos = 2;
+      }
+      let yPos = i % 3;
+
+      $li.data('pos', [xPos, yPos] );
       $ul.append($li);
     }
   }
