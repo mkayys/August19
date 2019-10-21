9e5e96cdbd90ca09fcb077dde0989158b1088a18
Michelle Kim <x1michelleskim@gmail.com>
2019-10-21 15:49:31 -0700

    Write populateList function to add list items of todos to the main ul

--- PocketProject/src/todo_list.js
+++ PocketProject/src/todo_list.js
@@ -1,2 +1,27 @@
+const todos = [];
 
+const ul = document.querySelector('.todos');
+const form = document.querySelector('.add-todo-form');
 
+const addTodo = function(e) {
+    const inputVal = document.querySelector('input[type=tex]').value;
+    let todo = {
+        text: inputVal,
+        done: false
+    }
+
+    todos.push(todo);
+
+    document.querySelector('input[type=tex]').value = "";
+};
+
+const populateList = function(todos) {
+    const todosArr = todos.map((todo) => {
+        return `<li>
+            <label>${todo.text}
+                <input type="checkbox" />
+            </label>
+        </li>`
+    });
+    todosArr.forEach(todo => ul.appendChild(todo));
+};
