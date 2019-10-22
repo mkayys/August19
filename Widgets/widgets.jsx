38d737abbc6a7fbcd5e8a15595829fcdd05abc03
Michelle Kim <x1michelleskim@gmail.com>
2019-10-22 09:38:40 -0700

    Create a root component to render a div

--- Widgets/widgets.jsx
+++ Widgets/widgets.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './root';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>React is WORKING</h1>, root);
+    ReactDOM.render(<Root />, root);
 })
