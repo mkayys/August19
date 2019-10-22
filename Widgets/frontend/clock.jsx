b0511138c995e0d9ef358123ee2852c39c87e51f
Michelle Kim <x1michelleskim@gmail.com>
2019-10-22 10:14:48 -0700

    Add clock that sets the interval and clears the interval on unmount

--- Widgets/frontend/clock.jsx
+++ Widgets/frontend/clock.jsx
@@ -3,15 +3,38 @@ import React from 'react';
 class Clock extends React.Component {
     constructor(props){
         super(props);
-        // this.state = {
-        //     time: new Date(),
-        // };
+        this.state = {
+            time: new Date(),
+        };
 
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    tick() {
+        this.setState({ time: new Date() });
+    }
+
+    timeString() {
+        let time = `${this.state.time.getHours()}:${this.state.time.getMinutes()}:${this.state.time.getSeconds()}`
+        return time;
     }
 
     render() {
         return (
-            <h1>clock</h1>
+            <div className='clock-container'>
+                <h1>clock</h1>
+                <div>
+                    {this.timeString()}
+                </div>
+            </div>
         )
     }
 };
