eee3b378e8d7b15267e74101402df1ffef656426
Michelle Kim <x1michelleskim@gmail.com>
2019-10-23 09:03:10 -0700

    Add minesweeper board and tile logic

---
+++ Minesweeper/components/game.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as Minesweeper from '../minesweeper';
+
+class Game extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Game;
