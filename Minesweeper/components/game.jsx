23bdc2c1eba29bc47a854ee9a8cce59bdfe7c8c1
Michelle Kim <x1michelleskim@gmail.com>
2019-10-23 14:43:12 -0700

    Finish game logic in Game component and add modal for game over

--- Minesweeper/components/game.jsx
+++ Minesweeper/components/game.jsx
@@ -13,14 +13,38 @@ class Game extends React.Component {
         this.updateGame = this.updateGame.bind(this);
     }
 
-    updateGame() {
+    updateGame(tile, flagging) {
+        if (flagging) {
+            tile.toggleFlag();
+        } else {
+            tile.explore();
+        }
+        this.setState({ board: this.state.board })
+    }
+
+    restartGame() {
 
     }
 
     render() {
+        const lost = (this.state.board.lost() ? "you lost" : "");
+        const win = (this.state.board.won() ? "you win": "");
+
+        const status = ((this.state.board.lost() || this.state.board.won()) ? "GAME OVER": "");
+        let modal;
+        if (lost || win) {
+            modal = 
+                <div className="modal-bck">
+                    <div className="modal-child">
+                        {/* {lost}{win} */}
+                        {status}
+                    </div>
+                </div>
+        }
         return (
             <div>
                 <h1>MINESWEEEPPPEEERR</h1>
+                {modal}
                 <Board board={this.state.board} updateGame={this.updateGame} />
             </div>
         )
--- Minesweeper/components/tile.jsx
+++ Minesweeper/components/tile.jsx
@@ -5,25 +5,43 @@ import * as Minesweeper from '../minesweeper';
 class Tile extends React.Component {
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        // console.log(e);
+        // debugger
+        const flagging = e.altKey ? true : false
+        console.log(flagging);
+        this.props.updateGame(this.props.tile, flagging);
     }
 
     render() {
         const { tile } = this.props;
-        let klass;
+        let klass = "none";
         let text;
-        if(tile.bombed) {
-            text = "💣";
-            klass = "bombed";
+        if (tile.explored) {
+            if (tile.bombed) {
+                text = "💣";
+                klass = "bombed";
+            } else {
+                text = `${tile.adjacentBombCount()}`;
+                klass = "revealed";
+            }
         } else if (tile.flagged) {
             text = "🏁";
             klass = "flagged";
-        } else if (tile.explored) {
-            text = `${tile.adjacentBombCount()}`;
-            klass = "revealed";
         }
+        // if (tile.bombed) {
+            
+        // } else if (tile.flagged) {
+            
+        // } else if (tile.explored) {
+            
+        // }
         return (
             <div className='tile'>
-                <div className={klass}>{text}</div>
+                <div className={klass} onClick={this.handleClick}>{text}</div>
             </div>
         )
     }
