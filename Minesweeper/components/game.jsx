9f8f79658b93f86241931eb7718eeb3c03b70447
Michelle Kim <x1michelleskim@gmail.com>
2019-10-23 09:12:07 -0700

    Create a game component that has a state of a new board

--- Minesweeper/components/game.jsx
+++ Minesweeper/components/game.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import * as Minesweeper from '../minesweeper';
 
 class Game extends React.Component {
-    constructor() {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: new Minesweeper.Board(5, 5)
+        }
     }
 
     render() {
