fc167005802b104829c210971c8850094d8d1094
Michelle Kim <x1michelleskim@gmail.com>
2019-10-23 09:27:25 -0700

    Create a tile component to be rendered by the board

---
+++ Minesweeper/components/tile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as Minesweeper from '../minesweeper';
+
+class Tile extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <div>T</div>
+        )
+    }
+}
+
+export default Tile;
