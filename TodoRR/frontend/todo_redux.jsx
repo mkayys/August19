8c32467f0bb9c01ed346d504a83cf855907be9d0
Michelle Kim <x1michelleskim@gmail.com>
2019-10-23 17:37:05 -0700

    Test rendering and setup of React

---
+++ TodoRR/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+    ReactDOM.render(<h1>Todo</h1>, content);
+})
