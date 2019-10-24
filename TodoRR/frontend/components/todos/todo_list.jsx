a7dd073778c0df826145a0dce0da2e7a5e242bdb
Michelle Kim <x1michelleskim@gmail.com>
2019-10-24 11:22:36 -0700

    Refactor each todo to be a list item component

--- TodoRR/frontend/components/todos/todo_list.jsx
+++ TodoRR/frontend/components/todos/todo_list.jsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
-export default () => <h3>Todo List goes here!</h3>
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const todos = this.props.todos.map(todo => {
+            return (
+                <TodoListItem key={todo.id} todo={todo} />
+            )
+        })
+        return (
+            <div>
+                <div>Todo List</div>
+                <ul>
+                    {todos}
+                </ul>
+            </div>
+        )
+    }
+};
+
+export default TodoList;
