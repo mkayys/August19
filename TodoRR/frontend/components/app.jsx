9404cda0ae644ef5654a3ca56cc139794f1462ad
Michelle Kim <x1michelleskim@gmail.com>
2019-10-24 11:09:16 -0700

    Add a simple presentational component and connect it with Redux through the container'

--- TodoRR/frontend/components/app.jsx
+++ TodoRR/frontend/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import TodoListContainer from './todos/todo_list_container';
 
 const App = () => {
     return (
-        <h1>Todo</h1>
+        <div>
+            <h1>Todo</h1>
+            <TodoListContainer />
+        </div>
     )
 }
 
