c344363c1a99a4ee0bbfd045d6855233710d5954
Michelle Kim <x1michelleskim@gmail.com>
2019-10-24 10:31:22 -0700

    Create a store and check the state with the inital state being passed in the todosReducer

--- TodoRR/frontend/todo_redux.jsx
+++ TodoRR/frontend/todo_redux.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const content = document.getElementById('content');
+
+    const store = configureStore();
+
+    window.store = store;
+
+
     ReactDOM.render(<h1>Todo</h1>, content);
 })
