1e06ebaab1e947ad3a5a2ee5d3c5688bd31a0077
Michelle Kim <x1michelleskim@gmail.com>
2019-10-24 13:36:34 -0700

    Change button to checkbox for done tasks

--- TodoRR/frontend/components/todos/todo_list_item.jsx
+++ TodoRR/frontend/components/todos/todo_list_item.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class TodoListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     todo: this.props.todo
+        // }
+        this.updateStatus = this.updateStatus.bind(this);
+    }
+
+    deleteTodo(todo) {
+        return (e) => {
+            this.props.removeTodo(todo)
+        }
+    }
+
+    updateStatus(e) {
+        const done = this.props.todo.done ? false : true;
+
+        console.log(done)
+        const todo = Object.assign({}, this.props.todo, { done });
+
+        this.props.receiveTodo(todo);
+    }
+
     render() {
         return (
             <li>
                 {this.props.todo.title}
+                <button onClick={this.deleteTodo(this.props.todo)}>BE GONE!</button>
+                <input type="checkbox" onClick={this.updateStatus} />
             </li>
         )
     }
