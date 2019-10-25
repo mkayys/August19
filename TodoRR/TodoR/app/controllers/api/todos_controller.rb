82be7937710a05b83a5c660419de3116e1364033
Michelle Kim <x1michelleskim@gmail.com>
2019-10-25 10:56:55 -0700

    Test api and routes with an ajax get request

--- TodoRR/TodoR/app/controllers/api/todos_controller.rb
+++ TodoRR/TodoR/app/controllers/api/todos_controller.rb
@@ -1,11 +1,13 @@
 class Api::TodosController < ApplicationController
 
     def index
+        @todos = Todo.all
+        render json: @todos
     end
 
     def destroy
     end
-    
+
     def show
         @todo = Todo.find(params[:id])
         render json: @todo
