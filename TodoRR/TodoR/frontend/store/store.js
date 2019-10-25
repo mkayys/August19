bb5e274af1830232d1253f16cceca1e97bdc9f7d
Michelle Kim <x1michelleskim@gmail.com>
2019-10-25 12:36:51 -0700

    Add thunk to middleware in store

--- TodoRR/TodoR/frontend/store/store.js
+++ TodoRR/TodoR/frontend/store/store.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import { thunk } from '../middleware/thunk';
 
 const configureStore = () => (
     createStore(
-        rootReducer
+        rootReducer,
+        applyMiddleware(thunk)
     )
 );
 
